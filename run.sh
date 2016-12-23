afb9eb17d1362ba8e7b947d0b98f46f0b8ede368
Mikhail Andreev <mikha.andreev@gmail.com>
2016-12-22 19:15:50 -0500

    Added basic robot environment

---
+++ run.sh
@@ -0,0 +1,6 @@
+PYTHONPATH="D:\\;D:\\Digital_Library\\data\\Documents\\Class\\EC700_Vulnerability_Malware_and_Defense_Analysis\\Projects\\repository\\Selenium Scripts\\wires-0.6.2-win;C:\\Python27\\Lib"
+export PYTHONPATH
+/c/Python27/python rgkit-master/rgkit-master/rgkit/run.py bots/example/robot.py bots/example/robot.py
+
+PYTHON_PATH="D:\\;D:\\Digital_Library\\data\\Documents\\Class\\EC700_Vulnerability_Malware_and_Defense_Analysis\\Projects\\repository\\Selenium Scripts\\wires-0.6.2-win;C:\\Program Files\\Python 3.5\\Lib\\;C:\\Python27\\Lib"
+export PYTHONPATH
